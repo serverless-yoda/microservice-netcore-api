0d0cf8df4c94712d1145207b4098a77264645298
manny i. agbayani <serverless.yoda@gmail.com>
2020-07-20 21:41:42 +1200

    added Delete and Get in BasketController

--- src/CodeCheater.Basket.API/Controllers/BasketController.cs
+++ src/CodeCheater.Basket.API/Controllers/BasketController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using CodeCheater.Application.Service;
+using CodeCheater.Domain.Models.Baskets;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Net;
 using System.Threading.Tasks;
-using CodeCheater.Application.Service;
-using CodeCheater.Domain.Models.Baskets;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace CodeCheater.Basket.API.Controllers
 {
@@ -15,24 +13,52 @@ namespace CodeCheater.Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketService service;
+        private readonly ILogger<BasketController> logger;
 
-        public BasketController(IBasketService service)
+        public BasketController(IBasketService service, ILogger<BasketController> logger)
         {
             this.service = service ?? throw new ArgumentNullException(nameof(service));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         [HttpPost]
-        [ProducesResponseType(typeof(BasketCart),(int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public async Task<ActionResult<BasketCart>> Post([FromBody] BasketCart basketCart)
         {
-            
+
             var result = await this.service.UpdateAsync(basketCart.UserName, basketCart);
-            if(result == null)
+            if (result == null)
             {
                 return BadRequest();
             }
             return Ok(result);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<BasketCart>> Get(string userName)
+        {
+            var result = await this.service.GetAsync(userName);
+            if (result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<bool>> Delete(string userName)
+        {
+            var result = await this.service.DeleteAsync(userName);
+            if (!result)
+            {
+                return BadRequest();
+            }
+            return Ok();
         }
     }
 }
