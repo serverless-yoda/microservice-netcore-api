4b17811fc3381d1dca44a26267a0a442f7340d51
manny i. agbayani <serverless.yoda@gmail.com>
2020-07-21 22:06:40 +1200

    added RuleFor of FluentValidator

--- src/CodeCheater.Application/RequestValidation/BasketInsertValidator.cs
+++ src/CodeCheater.Application/RequestValidation/BasketInsertValidator.cs
@@ -10,6 +10,7 @@ namespace CodeCheater.Application.RequestValidation
         public BasketInsertValidator(IServiceProvider provider)
         {
             RuleFor(x => x.UserName).NotNull().WithMessage("UserName is Empty");
+            RuleFor(x => x.UserName).NotNull().MinimumLength(3).WithMessage("Minimum length of Username is 3 characters");
             this.provider = provider ?? throw new ArgumentNullException(nameof(provider));
         }
     }
