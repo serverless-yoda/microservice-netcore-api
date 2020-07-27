1e46af2e5252432ea7fe8faf4ef837dcfc11da1d
manny i. agbayani <serverless.yoda@gmail.com>
2020-07-27 21:56:52 +1200

    added IBaseEFRepository

---
+++ src/CodeCheater.Domain/Repositories/BaseEFRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCheater.Domain.Repositories
+{
+    public class BaseEFRepository<T> : IBaseEFRepository<T> where T : Entity
+    {
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includedString = null, bool disableTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
