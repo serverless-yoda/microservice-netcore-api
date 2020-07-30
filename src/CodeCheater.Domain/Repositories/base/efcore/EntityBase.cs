4e0b4fea56f22bc93a2a6129c2ee018bbc5675b1
manny i. agbayani <serverless.yoda@gmail.com>
2020-07-31 09:11:45 +1200

    clean up

--- src/CodeCheater.Domain/Repositories/base/efcore/EntityBase.cs
+++ src/CodeCheater.Domain/Repositories/base/efcore/EntityBase.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CodeCheater.Domain.Repositories
 {
@@ -14,19 +13,13 @@ namespace CodeCheater.Domain.Repositories
         }
         public override bool Equals(object obj)
         {
-            if((obj == null) || !(obj is EntityBase<TId>))
-            {
-                return false;
-            }
-
+            if ((obj == null) || !(obj is EntityBase<TId>)) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (GetType() != obj.GetType()) return false;
-
+           
             var item = (EntityBase<TId>)obj;
-            if (item.IsTransient() || IsTransient())
-            {
-                return false;
-            }
+            if (item.IsTransient() || IsTransient()) return false;
+           
             return item == this;
         }
 
@@ -39,14 +32,10 @@ namespace CodeCheater.Domain.Repositories
         {
             if (!IsTransient())
             {
-                if (!_requestedHashCode.HasValue)
-                {
-                    _requestedHashCode = Id.GetHashCode() ^ 31;
-                }
+                if (!_requestedHashCode.HasValue) _requestedHashCode = Id.GetHashCode() ^ 31;
                 return _requestedHashCode.Value;
             }
-            else
-                return base.GetHashCode();
+            else return base.GetHashCode();
         }
 
         public static bool operator ==(EntityBase<TId> left, EntityBase<TId> right)
